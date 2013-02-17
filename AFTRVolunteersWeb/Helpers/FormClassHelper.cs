d11ac348fa6480de765e8ddcac4db616e505eed1
Scott Shaw <scottwshaw@gmail.com>
2013-02-17 19:48:36 +1100

    Changed some styling to go more bootstrappy

---
+++ AFTRVolunteersWeb/Helpers/FormClassHelper.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc.Html;
+
+namespace System.Web.Mvc
+{
+    public static class HtmlHelperExtension
+    {
+        public static MvcForm BeginFormWithClassName(this HtmlHelper helper, string cssClassName)
+        {
+            string controllerName = (string)helper.ViewContext.RouteData.Values["controller"];
+            string actionName = (string)helper.ViewContext.RouteData.Values["action"];
+            return helper.BeginForm(actionName, controllerName, FormMethod.Post, new { @class = cssClassName });
+        }
+    }
+}
